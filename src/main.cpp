58c7edd817992b9bf259740bbdf661cf1ca50428
Juul <juul20018@gmail.com>
2025-02-20 09:01:59 +0100

    removing some more lines

--- src/main.cpp
+++ src/main.cpp
@@ -119,9 +119,9 @@ void setup()
     lvgl_port_lock(-1);
 
     
-    lv_disp_t *dispp = lv_disp_get_default();
-    lv_theme_t *theme = lv_theme_default_init(dispp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), true, LV_FONT_DEFAULT);
-    lv_disp_set_theme(dispp, theme);
+    // lv_disp_t *dispp = lv_disp_get_default();
+    // lv_theme_t *theme = lv_theme_default_init(dispp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED), true, LV_FONT_DEFAULT);
+    // lv_disp_set_theme(dispp, theme);
 
     ui_FirstScreen = lv_obj_create(NULL);
     lv_obj_clear_flag( ui_FirstScreen, LV_OBJ_FLAG_SCROLLABLE );    /// Flags
